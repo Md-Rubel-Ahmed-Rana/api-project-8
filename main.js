19025f103a1d97e2a0626163da8197d696d5d4dc
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-30 23:24:51 +0600

    Data loaded successfully

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// https://openapi.programming-hero.com/api/phones?search=iphone
+
+// get the data from API or server
+const loadPhones = async() => {
+    const url = await `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
+
+
+
+
+loadPhones()
